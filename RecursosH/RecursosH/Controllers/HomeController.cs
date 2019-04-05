fcfdd86ac3ec48c421e33fc9650dcef78787d8cf
Proyecto final <lfelix3011@gmail.com>
2019-04-04 23:33:35 -0400

    procesos

--- RecursosH/RecursosH/Controllers/HomeController.cs
+++ RecursosH/RecursosH/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace RecursosH.Controllers
         {
             return View();
         }
+
+        public ActionResult Procesos()
+        {
+            return View();
+        }
     }
 }
